30b036c37e674b4ba4ebf32e03cbae9807020c85
kennethzhang <kennethzhang374@gmail.com>
2021-02-25 21:23:59 -0800

    set up routes using react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Experience from "./pages/Experience";
+import Projects from "./pages/Projects";
+import Reference from "./pages/Reference";
+
 function App() {
 	return (
-		<>
-			<p>this is the home page</p>
-		</>
+		<Router>
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route exact path="/about">
+					<About />
+				</Route>
+				<Route exact path="/contact">
+					<Contact />
+				</Route>
+				<Route exact path="/experience">
+					<Experience />
+				</Route>
+				<Route exact path="/projects">
+					<Projects />
+				</Route>
+				<Route exact path="/reference">
+					<Reference />
+				</Route>
+			</Switch>
+		</Router>
 	);
 }
 
