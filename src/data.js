506747fd33a58b896e150b23142cb6bc21b7b1de
kennethzhang <kennethzhang374@gmail.com>
2021-03-23 22:54:14 -0700

    uploaded a better img for WY

--- src/data.js
+++ src/data.js
@@ -91,7 +91,7 @@ let data = {
 	reference: [
 		{
 			id: 1,
-			image: "https://i.ibb.co/Q6mVmJR/willie.png",
+			image: "https://i.ibb.co/YTh5BmZ/WillY.png",
 			name: "William Yu",
 			title: "Production Manager in FLIR IIS",
 			quote: FirstQuote,
